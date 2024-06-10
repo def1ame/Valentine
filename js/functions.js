8ff143fa6d536df85bab1e63b8ca08338a99d0c0
def1ame <84040585+def1ame@users.noreply.github.com>
2024-06-10 14:41:18 +0600

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -103,7 +103,7 @@ function timeElapse(c) {
 	if (f < 10) {
 		f = "0" + f
 	}
-	var a = '<span class="digit">' + g + '</span> days <span class="digit">' + b + '</span> hours <span class="digit">' + d + '</span> minutes <span class="digit">' + f + "</span> seconds";
+	var a = '<span class="digit">' + g + '</span> Дней <span class="digit">' + b + '</span> часов <span class="digit">' + d + '</span> минут <span class="digit">' + f + "</span> секунд";
 	$("#elapseClock").html(a)
 }
 
